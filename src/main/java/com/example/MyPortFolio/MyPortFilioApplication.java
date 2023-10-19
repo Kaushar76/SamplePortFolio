93276d9a18d50f5c27caeb00abef9b4903acb666
Kausher <Kaushar01imam@gmail.com>
2023-10-19 15:18:29 +0530

    First Commit

---
+++ src/main/java/com/example/MyPortFolio/MyPortFilioApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyPortFolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortFilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortFilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyPortFilio/MyPortFilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyPortFilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortFilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
